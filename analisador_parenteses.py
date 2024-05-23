3fce498928156652fcc61f0f489eea27a4a38d97
TauaRibeiro <166765664+TauaRibeiro@users.noreply.github.com>
2024-05-23 09:51:34 -0300

    Update analisador_parenteses.py

--- analisador_parenteses.py
+++ analisador_parenteses.py
@@ -1,7 +1,7 @@
-#incompleto
 caracteres = []
-expressao = (input('Digite uma expressão: '))
 parenteses = 0
+
+expressao = (input('Digite uma expressão: '))
 
 for x in range(0, len(expressao)):
     caracteres.append(expressao[x:x+1])
@@ -9,10 +9,11 @@ for x in range(0, len(expressao)):
 for x in caracteres:
     if x == '(':
         parenteses += 1
-    elif x == ')' and parenteses >= 0:
-        parenteses -= 1
-    if parenteses <= 0:
-        parenteses -= 1
+    elif x == ')':
+        if parenteses >= 1:
+            parenteses -= 1
+        else:
+            parenteses -= 2
 
 if parenteses == 0:
     print('Expressão válida!!')
